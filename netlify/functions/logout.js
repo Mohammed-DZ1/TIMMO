52ced76b9de819d7c651ad445aa223491c2e25f8
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-24 22:22:16 +0100

    Delete netlify/functions directory

--- netlify/functions/logout.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-    return {
-        statusCode: 200,
-        headers: {
-            'Set-Cookie': 'authToken=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Strict',
-        },
-        body: JSON.stringify({ message: 'Logged out' }),
-    };
-};
