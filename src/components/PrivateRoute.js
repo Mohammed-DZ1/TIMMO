66324d3f75c20fd20ed04f44dc520197b43de923
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-25 02:51:22 +0100

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,8 +7,8 @@ const PrivateRoute = () => {
 
     if (loading) {
         return (
-            <div className="flex items-center justify-center h-screen">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary-500"></div>
+            <div className="flex items-center justify-center h-screen bg-secondary-500">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-primary-500"></div>
             </div>
         );
     }
