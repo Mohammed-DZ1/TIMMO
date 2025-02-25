0fccf0bc825821c1c88b87452b95e4e2c88b4c57
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-25 01:44:06 +0100

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth as authHook } from '../hooks/useAuth';
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useAuth();
+    const { user, loading } = authHook();
 
     if (loading) {
         return <div>Loading...</div>;
