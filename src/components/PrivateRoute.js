538fcaccf5d1786d14c43f668f9dbbf0a3ff2a97
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-25 04:05:00 +0100

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
 
     if (loading) {
-        return (
-            <div className="flex items-center justify-center min-h-screen bg-secondary-500">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-primary-500"></div>
-            </div>
-        );
+        return null;
     }
 
-    return user ? <Outlet /> : <Navigate to="/Login" replace />;
+    return user ? children : <Navigate to="/Login" replace />;
 };
 
 export default PrivateRoute;
