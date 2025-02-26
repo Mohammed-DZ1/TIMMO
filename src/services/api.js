08627cef6cbb685b5b03b61301f70176da4ac055
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-26 16:22:43 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,18 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 // Configure API base URL
 const api = axios.create({
     baseURL: 'https://timmodashboard.netlify.app/.netlify/functions/',
-    withCredentials: true
-});
-
-// Add Authorization header from cookie
-api.interceptors.request.use(config => {
-    const token = Cookies.get('authToken');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
     }
-    config.withCredentials = true;
-    return config;
 });
 
 // Login User
 export const loginUser = async (email, password) => {
     try {
         const response = await api.post('auth', { email, password });
-        // Set the token in a cookie that will be included in all requests
-        Cookies.set('authToken', response.data.token, { 
-            secure: true,
-            sameSite: 'strict',
-            path: '/'
-        });
         return response.data;
     } catch (error) {
         throw new Error(error.response?.data?.message || 'Login failed');
