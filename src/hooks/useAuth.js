3af40a9258aa4c182f0980077f800c31f07e1f63
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-26 17:53:39 +0100

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -59,10 +59,12 @@ export const AuthProvider = ({ children }) => {
     );
 };
 
-export const useAuth = () => {
+const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
 };
+
+export default useAuth;
