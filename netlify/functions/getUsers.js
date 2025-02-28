a1ea1ae9cffe8b964a0c215522dab275293dfcea
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-28 23:20:25 +0100

    Update getUsers.js

--- netlify/functions/getUsers.js
+++ netlify/functions/getUsers.js
@@ -1,12 +1,8 @@
-const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const { initializeFirebaseAdmin } = require('./utils/initializeFirebaseAdmin');
 
-// Initialize Firebase Admin
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-const app = initializeApp({
-    credential: cert(serviceAccount)
-}, 'getUsers');
-
+// Initialize Firebase Admin once
+initializeFirebaseAdmin();
 const db = getFirestore();
 
 exports.handler = async () => {
@@ -24,6 +20,8 @@ exports.handler = async () => {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': process.env.SITE_URL || '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Credentials': 'true'
             },
             body: JSON.stringify(users)
@@ -35,6 +33,8 @@ exports.handler = async () => {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': process.env.SITE_URL || '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Credentials': 'true'
             },
             body: JSON.stringify({ error: 'Internal server error' })
