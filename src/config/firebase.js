04cebc803daf9232c8e28bc0e9b6feb4911de8de
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-02-28 14:42:29 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyC9fZZCxZvI-qY6UXPTKh0LAWruB-qIEVI",
     authDomain: "timmo-2f70c.firebaseapp.com",
     projectId: "timmo-2f70c",
-    storageBucket: "timmo-2f70c.firebasestorage.app",
+    storageBucket: "timmo-2f70c.appspot.com",
     messagingSenderId: "1075188777095",
     appId: "1:1075188777095:web:9b5b2b15677634fa5db822",
     measurementId: "G-16Q56WEB16"
