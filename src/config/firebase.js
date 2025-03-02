35ff522ad53c0640f8326d84fc61968307fa043f
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-03-02 03:32:19 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9fZZCxZvI-qY6UXPTKh0LAWruB-qIEVI",
-    authDomain: "timmo-2f70c.firebaseapp.com",
-    projectId: "timmo-2f70c",
-    storageBucket: "timmo-2f70c.appspot.com",
-    messagingSenderId: "1075188777095",
-    appId: "1:1075188777095:web:9b5b2b15677634fa5db822",
-    measurementId: "G-16Q56WEB16"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
