ccb4c97c4bdde137cf2c752c86e3d24d7f248a2a
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-03-05 03:59:49 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,20 +3,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
-// Use Netlify environment variables
 const firebaseConfig = {
     apiKey: "AIzaSyC9fZZCxZvI-qY6UXPTKh0LAWruB-qIEVI",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    authDomain: "timmo-2f70c.firebaseapp.com",
+    projectId: "timmo-2f70c",
+    storageBucket: "timmo-2f70c.appspot.com",
+    messagingSenderId: "1075188777095",
+    appId: "1:1075188777095:web:9b5b2b15677634fa5db822",
+    measurementId: "G-16Q56WEB16"
 };
 
-// Debugging: Check if variables are loaded from Netlify
-console.log("Firebase Config from Netlify:", firebaseConfig);
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
